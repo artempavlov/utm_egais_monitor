5bba200a7017a2a593f66a715a8a25691de81e33
artempavlov <art.pavlov.2012@gmail.com>
2020-11-24 14:39:53 +0300

    refactor

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from api import API
 import time
 from pebble import ThreadPool, concurrent
-from prometheus_client import start_http_server, Info, Gauge
+from prometheus_client import start_http_node, Info, Gauge
 
 
-class Server(object):
+class Node(object):
     def __init__(self, ip, port):
         self.api = API(ip, port)
-        self.up = Gauge(('up' + ip + ':' + port).replace('.', '_').replace(':', '_'), 'Up')
-        #self.up = Gauge('asdf', 'Up')
+        self.up = Gauge(('up' + ip + ':' + port).replace('.', '_'), 'Up')
 
     def update(self):
         self.api.update()
@@ -17,10 +16,10 @@ class Server(object):
 
 class Monitor(object):
     def __init__(self):
-        self.servers = []
+        self.nodes = []
         addresses = self.load_addresses()
         for address in addresses:
-            self.servers.append(Server(address[0], address[1]))
+            self.nodes.append(Node(address[0], address[1]))
 
     def load_addresses(self):
         with open('addresses.txt') as f:
@@ -28,26 +27,26 @@ class Monitor(object):
         return addresses
 
     def run(self):
-        self.run_prometheus_server()
+        self.run_prometheus_node()
         while True:
             pool = ThreadPool(max_workers=100)
             status = []
-            for server in self.servers:
-                pool.schedule(server.update)
+            for node in self.nodes:
+                pool.schedule(node.update)
             pool.close()
             pool.join()
-            for server in self.servers:
-                if server.up:
-                    status.append([server.api.ip+':'+server.api.port, server.up, server.api.rsa_certificate_expiry_date])
+            for node in self.nodes:
+                if node.up:
+                    status.append([node.api.ip+':'+node.api.port, node.up, node.api.rsa_certificate_expiry_date])
                 else:
-                    status.append([server.api.ip+':'+server.api.port, server.api.up])
-                print(server.api.ip+':'+server.api.port, server.api.up, server.api.rsa_certificate_expiry_date)
+                    status.append([node.api.ip+':'+node.api.port, node.api.up])
+                print(node.api.ip+':'+node.api.port, node.api.up, node.api.rsa_certificate_expiry_date)
             print(status)
-            time.sleep(30)
+            time.sleep(5)
 
     @concurrent.thread
-    def run_prometheus_server(self):
-        start_http_server(38000)
+    def run_prometheus_node(self):
+        start_http_node(38000)
 
 
 if __name__ == '__main__':
